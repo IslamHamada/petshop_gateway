e1dc4543ee671cd7bbff4b08ca765071ef52f129
IslamHamada <Islamhamada222@gmail.com>
2025-11-25 20:12:42 +0100

    inject my.frontend.url in frontend_url to prevent cors from blocking it

--- src/main/java/com/islamhamada/petshop/security/CorsConfig.java
+++ src/main/java/com/islamhamada/petshop/security/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.islamhamada.petshop.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${my.frontend.url}")
+    private String frontend_url;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8090")
+                .allowedOrigins(frontend_url)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
