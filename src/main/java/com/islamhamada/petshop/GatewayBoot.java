fe0bfb5351dcf9e5a65d4b9b4fe91389bb98cae8
IslamHamada <islamhamada222@gmail.com>
2026-01-26 19:07:03 +0100

    add Circuitbreaker config bean

--- src/main/java/com/islamhamada/petshop/GatewayBoot.java
+++ src/main/java/com/islamhamada/petshop/GatewayBoot.java
@@ -1,7 +1,12 @@
 package com.islamhamada.petshop;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayBoot {
@@ -10,4 +15,10 @@ public class GatewayBoot {
 		SpringApplication.run(GatewayBoot.class, args);
 	}
 
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> resilience4JCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.build());
+	}
 }
